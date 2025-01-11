a61df1d13a16dc1f88903e6158db9a45a2c18b2d
Michael Pfeil <gamermaster2115@gmail.com>
2025-01-11 15:09:24 +0100

    Init full website

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Small JS to show/hide the mobile menu
+const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
+const mobileNavContainer = document.querySelector(".mobile-nav-container");
+mobileNavToggle.addEventListener("click", () => {
+  mobileNavContainer.classList.toggle("open");
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    const targetID = this.getAttribute('href').substring(1);
+    const targetEl = document.getElementById(targetID);
+    if (targetEl) {
+      e.preventDefault();
+      targetEl.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Scroll-to-top button
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 1) {
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+// Click -> Smooth scroll up
+scrollTopBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
